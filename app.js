2d19cffb34746aa7cba1abbbb33c78d4d74d835d
Matthew <matthew@Matthews-MacBook-Air.local>
2026-03-11 14:09:52 -0700

    Module 2 MVC conversion

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const path = require('path');
+const handlebars = require('hbs');
+
+const indexRouter = require('./app_server/routes/index');
+const travelRouter = require('./app_server/routes/travel');
 
 const app = express();
 const port = 3000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+// register handlebars partials
+handlebars.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
+
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
+
+// start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
