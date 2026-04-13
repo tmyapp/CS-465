d51e8f7a72e60304314bd82305812637eb7ffb33
Matthew <matthew@Matthews-MacBook-Air.local>
2026-04-12 19:53:48 -0700

    Module 6 start - Angular admin app setup

---
+++ travlr-admin/src/app/trip-card/trip-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  imports: [],
+  templateUrl: './trip-card.html',
+  styleUrl: './trip-card.css',
+})
+export class TripCard {}
---
+++ travlr-admin/src/app/trip-list/trip-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-list',
+  imports: [],
+  templateUrl: './trip-list.html',
+  styleUrl: './trip-list.css',
+})
+export class TripList {}
